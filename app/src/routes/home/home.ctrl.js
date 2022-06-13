68fdbf63d2ac7327f8cd0df62d658f6e0d0551e1
shinsoonbum <ccmnk@naver.com>
2022-06-13 16:38:56 +0900

    회원가입라우팅 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
-
+const User = require("../../models/Users");
 
 const output = {
     home : (req, res) => {
@@ -10,25 +9,20 @@ const output = {
     login : (req,res) => {
         res.render('home/login')
     },
+    register : (req,res) => {
+        res.render("home/register");
+    }
 };
 
 const process = {
     login : (req,res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response ={} ;
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword){
-                response.success = true; 
-                return res.json(response);
-            }
-        }
-        response.success  = false;
-        response.msg = "로그인에 실패하였습니다.";
+        const user = new User(req.body);
+        const response =user.login();
+        return res.json(response);
+    },
+    register : (req,res) => {
+        const user = new User(req.body);
+        const response =user.register();
         return res.json(response);
     },
 };
